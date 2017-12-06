dfb74c18df9d0add8837af82844981c2300b2c56
infyhr <infy@cybershade.org>
2017-12-06 20:21:19 +0100

    Made carousel buttons functional.

--- src/components/basic/Game/ImageList.jsx
+++ src/components/basic/Game/ImageList.jsx
@@ -11,10 +11,33 @@ class ImageList extends React.Component {
     constructor() {
         super();
 
-        this.handleClick = this.handleClick.bind(this);
+        this.handleThumbnailClick = this.handleThumbnailClick.bind(this);
     }
 
-    handleClick(e) {
+    componentDidMount() {
+        this.setState({selectedImage: this.props.images[0]});
+    }
+
+    handleButtonClick(direction) {
+        // First let's find the current image index
+        let currentIndex = this.props.images.findIndex(el => el == this.state.selectedImage);
+        console.log('Current index: ' + currentIndex);
+
+        let newIndex = 0;
+
+        switch(direction) {
+            case 'right':
+                newIndex = (currentIndex + 1 > this.props.images.length - 1) ? 0 : currentIndex + 1;
+            break;
+            case 'left':
+                newIndex = (currentIndex - 1 < 0) ? this.props.images.length - 1 : currentIndex - 1;
+            break;
+        }
+
+        this.setState({selectedImage: this.props.images[newIndex]});
+    }
+
+    handleThumbnailClick(e) {
         // Custom attributes aren't supported in React15, so we have to do this dirty trick.
         // I mean, probably there exist a better solution. Alas, this is what I had come up with.
         // Whatever...
@@ -29,8 +52,15 @@ class ImageList extends React.Component {
         return (
             <div className="ImageList">
                 <div className="ImageList__mainImage" style={{background: 'url(' + mainImage + ') center / cover'}}>
-                    <i className="fa fa-chevron-circle-left ImageList__mainImage-button ImageList__mainImage-button--left"  aria-hidden="true"></i>
-                    <i className="fa fa-chevron-circle-right ImageList__mainImage-button ImageList__mainImage-button--right" aria-hidden="true"></i>
+                    <i 
+                        className="fa fa-chevron-circle-left ImageList__mainImage-button ImageList__mainImage-button--left"
+                        aria-hidden="true"
+                        onClick={() => this.handleButtonClick('left')}>
+                    </i>
+                    <i className="fa fa-chevron-circle-right ImageList__mainImage-button ImageList__mainImage-button--right"
+                       aria-hidden="true"
+                       onClick={() => this.handleButtonClick('right')}>
+                    </i>
                 </div>
                 <div className="ImageList__images">
                     {
@@ -44,7 +74,7 @@ class ImageList extends React.Component {
                                      key={image}
                                      style={{background: 'url(' + image + ') center / cover'}}
                                      className={imageClasses}
-                                     onClick={this.handleClick}>
+                                     onClick={this.handleThumbnailClick}>
                                 </div>
                             )  
                         })
